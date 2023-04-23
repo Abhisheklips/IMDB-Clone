2574ca40f5798e50ffac6daa6e3137e138f4fca4
Abhisheklips <chandravanshiabhishek72@gmail.com>
2023-04-23 10:13:25 +0530

    error message add

--- src/app/error.js
+++ src/app/error.js
@@ -7,7 +7,7 @@ const Error = ({ error, reset }) => {
   }, { error })
   return (
     <div className='text-center mt-10'>
-      <h1>something werong</h1>
+      <h1>Something wrong</h1>
       <button className='hover:text-amber-500' onClick={() => reset()}> try again</button>
     </div>
   )
